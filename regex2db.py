8822708ab897af028c7e4d32778a77afb81208a6
Michael Nagel <michael.nagel@devzero.de>
2015-02-07 16:37:55 +0100

    use logging framework

--- regex2db.py
+++ regex2db.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 
 import argparse
+import logging
 import re
 import sqlite3
 import sys
@@ -18,13 +19,16 @@ def read_arguments(args):
     parser.add_argument('--capture', nargs=3, action='append')
     parser.add_argument('infile', nargs='+')
 
+    parser.add_argument('-d','--debug', action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.WARNING)
+    parser.add_argument('-v','--verbose', action="store_const",dest="loglevel", const=logging.INFO)
+
     options = parser.parse_args(args)
     return options
 
 def processLine(line, con, cur):
     m = re.match(options.regex, line)
     if m:
-        print("matched line %s" % line.rstrip())
+        logging.debug("matched line %s" % line.rstrip())
         # print(m)
         pairs = {}
         for cap in options.capture:
@@ -32,7 +36,7 @@ def processLine(line, con, cur):
             val = m.group(idx)
             tfx = cap[1]
             col = cap[2]
-            print("appling tfx %s to val %s and putting it into col %s" % (tfx, val, col))
+            logging.debug("appling tfx %s to val %s and putting it into col %s" % (tfx, val, col))
             tfxval = val # TODO fixme
             pairs[col] = tfxval
 
@@ -44,7 +48,7 @@ def writeLineToDb(con, cur, table, col_val_dict):
     columns = ', '.join(col_val_dict.keys())
     placeholders = ':' + ( ', :'.join(col_val_dict.keys()) )
     query = 'INSERT INTO %s (%s) VALUES (%s)' % (table, columns, placeholders)
-    print('qry: %s' % query)
+    logging.debug('qry: %s' % query)
     cur.execute(query, col_val_dict)
 
 def processFile(file, con, cur):
@@ -53,26 +57,27 @@ def processFile(file, con, cur):
             for line in f:
                 if processLine(line, con, cur):
                     matchcount += 1
-        print("matched %d lines" % matchcount)
+        logging.info("matched %d lines" % matchcount)
 
 def main(options):
-    print("files are: %s" % options.infile)
-    print("captures are: %s" % options.capture)
+    logging.info("files are: %s" % options.infile)
+    logging.info("captures are: %s" % options.capture)
 
     con = sqlite3.connect(options.dbfile)
     cur = con.cursor()
 
     if options.truncate:
-        print("!!! TRUNCATING !!!")
+        logging.warning("!!! TRUNCATING !!!")
         # TODO check the whole program for major sql injections
         cur.execute('DELETE FROM %s' % options.tablename)
 
     for file in options.infile:
-        print("processing file %s" % file)
+        logging.info("processing file %s" % file)
         processFile(file, con, cur)
 
     con.commit()
 
 if __name__ == '__main__':
     options = read_arguments(sys.argv[1:])
+    logging.basicConfig(level=options.loglevel)
     main(options)
