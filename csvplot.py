a3e79a424c35d2d327113d8b7a6dab7f08b5576b
Michael Nagel <michael.nagel@devzero.de>
2015-02-08 22:19:38 +0100

    new feature: sql plotting

--- csvplot.py
+++ csvplot.py
@@ -12,54 +12,81 @@ import matplotlib
 #import matplotlib.pyplot as plot
 import numpy
 import re
+import sqlite3
+
+def add_common_arguments(parser):
+    parser.add_argument('--title', default="simple plot by csvplot", type=str)
+    parser.add_argument('--xsize', default=8, type=int)
+    parser.add_argument('--xtransform', default="float", type=str)
+    parser.add_argument('--xlog', default=False, action="store_true")
+    parser.add_argument('--xlabel', default="x-values", type=str)
+    parser.add_argument('--ysize', default=6, type=int)
+    parser.add_argument('--ytransform', default="float", type=str)
+    parser.add_argument('--ylog', default=False, action="store_true")
+    parser.add_argument('--ylabel', default="y-values", type=str)
+    parser.add_argument('--outfile', default=None, type=str)
+    parser.add_argument('--show', default=False, action="store_true")
+    parser.add_argument('--marker', default='.', type=str)
+    parser.add_argument('--linestyle', default='', type=str)
+    parser.add_argument('--interact', default=False, action="store_true")
+    parser.add_argument('--nolatex', default=False, action="store_true")
+    parser.add_argument('--dateformat', default='%Y-%m-%d@%H:%M:%S', type=str)
+    parser.add_argument('--datelocator', default="auto", type=str)
 
 parser = argparse.ArgumentParser(prog='csvplot')
-parser.add_argument('--title', default="simple plot by csvplot", type=str)
-parser.add_argument('--x', default=1, type=int)
-parser.add_argument('--xsize', default=8, type=int)
-parser.add_argument('--xtransform', default="float", type=str)
-parser.add_argument('--xlog', default=False, action="store_true")
-parser.add_argument('--xlabel', default="x-values", type=str)
-parser.add_argument('--y', default=2, type=int)
-parser.add_argument('--ysize', default=6, type=int)
-parser.add_argument('--ytransform', default="float", type=str)
-parser.add_argument('--ylog', default=False, action="store_true")
-parser.add_argument('--ylabel', default="y-values", type=str)
-parser.add_argument('--infile', default=None, type=str)
-parser.add_argument('--outfile', default=None, type=str)
-parser.add_argument('--show', default=False, action="store_true")
-parser.add_argument('--marker', default='.', type=str)
-parser.add_argument('--linestyle', default='', type=str)
-parser.add_argument('--interact', default=False, action="store_true")
-parser.add_argument('--sep', default=",", type=str)
-parser.add_argument('--nolatex', default=False, action="store_true")
-parser.add_argument('--dateformat', default='%Y-%m-%d@%H:%M:%S', type=str)
-parser.add_argument('--datelocator', default="auto", type=str)
+
+subparsers = parser.add_subparsers(dest="operationmode")
+sql_parser = subparsers.add_parser('sqlmode')
+sql_parser.add_argument('--dbfile', required=True, type=str)
+sql_parser.add_argument('--sql', required=True, type=str)
+add_common_arguments(sql_parser)
+
+csv_parser = subparsers.add_parser('csvmode')
+csv_parser.add_argument('--sep', default=",", type=str)
+csv_parser.add_argument('--x', dest='x', default=1, type=int)
+csv_parser.add_argument('--y', dest='y', default=2, type=int)
+csv_parser.add_argument('--infile', default=None, type=str)
+add_common_arguments(csv_parser)
 
 options = parser.parse_args()
+
+if options.operationmode == "csvmode":
+    # csvplot columns are indexed 1-based
+    options.x = options.x - 1
+    options.y = options.y - 1
+
+    if options.infile is None:
+        exit("--infile argument is mandatory")
+
+    if options.sep == '\\t':
+        options.sep = '\t'
+
+    r = csv.DictReader(open(options.infile), delimiter=options.sep)
+    # index to name
+    i2n = r.fieldnames
+
+elif options.operationmode == "sqlmode":
+    def dict_factory(cursor, row):
+        d = {}
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+    print('connecting to %s' % options.dbfile)
+    con = sqlite3.connect(options.dbfile)
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    print('execing: %s' % options.sql)
+    rs = cur.execute(options.sql)
 
 # need options before matplotlib can be imported
 if not options.show:
     matplotlib.use('Agg') # allow creating pngs without X server
 import matplotlib.pyplot as plot
 
-# csvplot columns are indexed 1-based
-options.x = options.x - 1
-options.y = options.y - 1
-
 numpy.set_printoptions(linewidth=200)
 numpy.set_printoptions(suppress=True)
 numpy.set_printoptions(precision=5)
-
-if options.infile is None:
-    exit("--infile argument is mandatory")
-
-if options.sep == '\\t':
-    options.sep = '\t'
-
-r = csv.DictReader(open(options.infile), delimiter=options.sep)
-# index to name
-i2n = r.fieldnames
 
 # TODO move the supported transformers elsewhere
 def transform(value, transformation):
@@ -79,14 +106,30 @@ def transform(value, transformation):
             return None
     return value
 
+if options.operationmode == "csvmode":
+    allmylines = r
+    xind = i2n[options.x]
+    yind = i2n[options.y]
+elif options.operationmode == "sqlmode":
+    allmylines = iter(rs.fetchone, None)
+    xind = 'x'
+    yind = 'y'
+
+print('fetching')
 x,y = [],[]
-for line in r:
-    xval = line[i2n[options.x]]
-    yval = line[i2n[options.y]]
+cnt = 0
+for res in allmylines:
+    cnt += 1
+    if not cnt % 1000:
+        print("got to iteration %d" % cnt)
+        print(res)
+
+    xval = res[xind]
+    yval = res[yind]
     xt = transform(xval, options.xtransform)
     yt = transform(yval, options.ytransform)
     if xt is None or yt is None:
-        print("could not process line %s" % line)
+        print("could not process line %s" % res)
         continue
     x.append(xt)
     y.append(yt)
